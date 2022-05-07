d0a965f24db16dc6bfcf75104475b546027b41c6
sshwy <jy.cat@qq.com>
2022-05-07 15:13:36 +0800

    feat: implement /register command

--- main/login_service.go
+++ main/login_service.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/Project-Nessie/nessielight/service"
 	"github.com/Project-Nessie/nessielight/tgolf"
 	"github.com/yanzay/tbot/v2"
 )
 
 func registerLoginService(server *tgolf.Server) {
-	// !!!UNIMPLEMENTED
 	server.Register("/register", "Register yourself", func(from *tbot.User, chat tbot.Chat) bool {
-		return from != nil && chat.Type == "private"
-	}, nil, func(argv []tgolf.Argument, from *tbot.User, chatid string) {
-		server.Sendf(chatid, "<i>register service not implemented</i>")
+		if from == nil || chat.Type != "private" {
+			server.Sendf(chat.ID, "invalid environment")
+			return false
+		}
+		id := fmt.Sprint(from.ID)
+		user, err := service.UserManagerInstance.FindUser(id)
+		if err != nil {
+			server.Sendf(chat.ID, err.Error())
+			return false
+		}
+		if user != nil {
+			server.Sendf(chat.ID, "You've already registered")
+			return false
+		}
+		return true
+	}, []tgolf.Parameter{
+		tgolf.NewParam("token", "token", nil),
+	}, func(argv []tgolf.Argument, from *tbot.User, chatid string) {
+		token := argv[0].Value
+		if _, err := service.AuthServiceInstance.Register(token); err != nil {
+			server.Sendf(chatid, "register failed: %s", err.Error())
+			return
+		}
+		server.Sendf(chatid, "Register succeed.")
 	})
 }
